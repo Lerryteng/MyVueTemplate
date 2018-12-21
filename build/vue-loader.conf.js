9e49cf5cb65583b226e2538889289b8ed8845e8b
Lerryteng <wangyiteng2927@163.com>
2018-12-21 15:18:07 +0800

    add pxtorem

--- build/vue-loader.conf.js
+++ build/vue-loader.conf.js
@@ -1,6 +1,7 @@
 'use strict'
 const utils = require('./utils')
 const config = require('../config')
+const pxtorem = require('postcss-pxtorem')
 const isProduction = process.env.NODE_ENV === 'production'
 const sourceMapEnabled = isProduction
   ? config.build.productionSourceMap
@@ -18,5 +19,28 @@ module.exports = {
     source: 'src',
     img: 'src',
     image: 'xlink:href'
-  }
+  },
+  postcss: [pxtorem({
+    rootValue: 37.5,
+    unitPrecision: 5,
+    propList: ['font',
+    'font-size',
+    'line-height',
+    'letter-spacing',
+    'height',
+    'min-height',
+    'width',
+    'min-width',
+    'padding*',
+    'margin*',
+    'top',
+    'left',
+    'right',
+    'bottom',
+    'border*'],
+    selectorBlackList: [],
+    replace: true,
+    mediaQuery: false,
+    minPixelValue: 0
+  })]
 }
